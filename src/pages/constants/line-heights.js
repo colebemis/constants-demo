3b94e26f2cd32c7688c9e4d0623d322236416f9a
Cole Bemis <colebemis@gmail.com>
2018-06-01 17:07:30 -0700

    Add line heights

---
+++ src/pages/constants/line-heights.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Box from '../../components/Box';
+import Code from '../../components/Code';
+import Heading from '../../components/Heading';
+import Table, { Cell, Header, Row } from '../../components/Table';
+import { lineHeights } from '../../constants';
+
+export default props => (
+   <Box>
+      <Heading>Line Weights</Heading>
+
+      <Table>
+         <Header>
+            <Row>
+               <Cell is="th">Key</Cell>
+               <Cell is="th">Value</Cell>
+            </Row>
+         </Header>
+         <tbody>
+            {Object.entries(lineHeights).map(([key, value]) => (
+               <Row key={key}>
+                  <Cell>
+                     <Code>lineHeights.{key}</Code>
+                  </Cell>
+                  <Cell>
+                     <Code>{value}</Code>
+                  </Cell>
+               </Row>
+            ))}
+         </tbody>
+      </Table>
+   </Box>
+);
