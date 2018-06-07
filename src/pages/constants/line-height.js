7844321bdb6b7628041f6712899cc4322dbd50ad
Cole Bemis <colebemis@gmail.com>
2018-06-07 11:36:59 -0700

    Add line height example

--- src/pages/constants/line-height.js
+++ src/pages/constants/line-height.js
@@ -3,12 +3,23 @@ import React from 'react';
 import Box from '../../components/Box';
 import Heading from '../../components/Heading';
 import KeyValueTable from '../../components/KeyValueTable';
+import Paragraph from '../../components/Paragraph';
 
 const { lineHeight } = constants;
 
 export default props => (
    <Box>
       <Heading>Line Height</Heading>
+
+      {Object.entries(lineHeight).map(([key, value]) => (
+         <Paragraph lineHeight={value}>
+            {key} ({value}) Lorem ipsum dolor sit amet, consetetur sadipscing
+            elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+            magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+            justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+            takimata sanctus est Lorem ipsum dolor sit amet.
+         </Paragraph>
+      ))}
 
       <KeyValueTable name="lineHeight" data={lineHeight} />
    </Box>
